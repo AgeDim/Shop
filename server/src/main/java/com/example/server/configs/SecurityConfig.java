3e3df0abf0e35b024c208673f6645cb17482e59c
1MikhailStepanov1 <wakamail923@gmail.com>
2022-11-12 20:01:02 +0300

    Fixing CORS. Try 1

--- server/src/main/java/com/example/server/configs/SecurityConfig.java
+++ server/src/main/java/com/example/server/configs/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.server.configs;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class SecurityConfig extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
     }
 }
