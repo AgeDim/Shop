8a881f30c261c2d7b38fa4889b59d44afa84c81e
AgeDim <proga778@gmail,com>
2022-11-22 04:16:29 +0300

    авторизация

--- server/src/main/java/com/example/server/POJO/AuthRequest.java
+++ server/src/main/java/com/example/server/POJO/AuthRequest.java
@@ -4,7 +4,6 @@ public class AuthRequest {
 
     private String email;
     private String password;
-    private String username;
 
     public String getEmail() {
         return email;
@@ -22,11 +21,4 @@ public class AuthRequest {
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
--- server/src/main/java/com/example/server/controllers/UserController.java
+++ server/src/main/java/com/example/server/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.example.server.POJO.AuthRequest;
 import com.example.server.exceptions.NotHavePermissionException;
 import com.example.server.exceptions.UserAlreadyExistsException;
 import com.example.server.exceptions.WrongPasswordException;
+import com.example.server.security.JwtUtils;
 import com.example.server.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,34 +18,35 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    JwtUtils jwtUtils;
+
     //TODO Delete printing traces
     @PostMapping("/reg")
-    public ResponseEntity<?> register (@RequestBody AuthRequest user){
+    public ResponseEntity<?> register(@RequestBody AuthRequest user) {
         try {
             userService.register(user);
-            return ResponseEntity.ok(true);
-        } catch (UserAlreadyExistsException e){
+            return ResponseEntity.ok(jwtUtils.generateToken(user.getEmail()));
+        } catch (UserAlreadyExistsException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body("An error occurred on the server");
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthRequest user){
+    public ResponseEntity<?> login(@RequestBody AuthRequest user) {
         try {
             userService.login(user);
-            return ResponseEntity.ok(true);
-        } catch (UserAlreadyExistsException | WrongPasswordException e){
+            return ResponseEntity.ok(jwtUtils.generateToken(user.getEmail()));
+        } catch (UserAlreadyExistsException | WrongPasswordException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e){
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body("Ошибка.");
         }
     }
+
     @GetMapping("/checkModer/{email}")
-    public ResponseEntity<?> checkModerRights(@PathVariable String email){
+    public ResponseEntity<?> checkModerRights(@PathVariable String email) {
         try {
             userService.checkModerRights(email);
             return ResponseEntity.ok("Права доступа верны.");
@@ -52,14 +54,21 @@ public class UserController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
     @GetMapping("/checkAdmin/{email}")
-    public ResponseEntity<?> checkAdminRights(@PathVariable String email){
+    public ResponseEntity<?> checkAdminRights(@PathVariable String email) {
         try {
             userService.checkAdminRights(email);
             return ResponseEntity.ok("Права доступа верны.");
         } catch (NotHavePermissionException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    //Я не понимаю
+    @GetMapping("/check")
+    public ResponseEntity<?> check(@PathVariable String email){
+        return ResponseEntity.ok(jwtUtils.generateToken(email));
     }
 
 }
