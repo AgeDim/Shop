3a8ef0937fb94c2c6b2ad82ce688dcd32b3480e6
1MikhailStepanov1 <wakamail923@gmail.com>
2022-12-07 21:32:52 +0300

    Added converting of image from db format to user format.

---
+++ server/src/main/java/com/example/server/POJO/ProductResponse.java
@@ -0,0 +1,90 @@
+package com.example.server.POJO;
+
+import com.example.server.entities.enums.ProductType;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private ProductType productType;
+
+    private Double defaultPrice;
+
+    private String description;
+
+    private String imgName;
+
+    private MultipartFile img;
+
+    public ProductResponse(){}
+
+    public ProductResponse(Long id, String name, Integer productType, Double defaultPrice, String description,
+                           String imgName, MultipartFile img){
+        this.id = id;
+        this.name = name;
+        this.productType = ProductType.values()[productType];
+        this.defaultPrice = defaultPrice;
+        this.description = description;
+        this.imgName = imgName;
+        this.img = img;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    public void setProductType(ProductType productType) {
+        this.productType = productType;
+    }
+
+    public Double getDefaultPrice() {
+        return defaultPrice;
+    }
+
+    public void setDefaultPrice(Double defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgName() {
+        return imgName;
+    }
+
+    public void setImgName(String imgName) {
+        this.imgName = imgName;
+    }
+
+    public MultipartFile getImg() {
+        return img;
+    }
+
+    public void setImg(MultipartFile img) {
+        this.img = img;
+    }
+}
--- server/src/main/java/com/example/server/services/ProductService.java
+++ server/src/main/java/com/example/server/services/ProductService.java
@@ -1,11 +1,13 @@
 package com.example.server.services;
 
 import com.example.server.POJO.ProductRequest;
+import com.example.server.POJO.ProductResponse;
 import com.example.server.entities.ProductEntity;
 import com.example.server.entities.enums.ProductType;
 import com.example.server.repositories.ProductRepository;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
 
@@ -24,12 +27,18 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<ProductEntity> getProductTop(){
-        return productRepository.getTopProducts();
+    public List<ProductResponse> getProductTop(){
+        List<ProductEntity> temp = productRepository.getTopProducts();
+        List<ProductResponse> result = new ArrayList<>();
+        convertEntityToResponse(temp, result);
+        return result;
     }
 
-    public List<ProductEntity> getProductByType(String type) throws IllegalArgumentException{
-        return productRepository.getProductEntitiesByProductType(ProductType.valueOf(type).getCode());
+    public List<ProductResponse> getProductByType(String type) throws IllegalArgumentException{
+        List<ProductEntity> temp = productRepository.getProductEntitiesByProductType(ProductType.valueOf(type).getCode());
+        List<ProductResponse> result = new ArrayList<>();
+        convertEntityToResponse(temp, result);
+        return result;
     }
 
     public ProductEntity addProduct(ProductRequest request) throws IOException {
@@ -59,5 +68,15 @@ public class ProductService {
         fos.write(file.getBytes());
         fos.close();
         return result;
+    }
+
+    private MultipartFile convertBytesToMultipartFile(byte[] fileInBytes, String name){
+        return new MockMultipartFile(name, name, "image/jpeg", fileInBytes);
+    }
+
+    private void convertEntityToResponse(List<ProductEntity> entityList, List<ProductResponse> responseList){
+        entityList.forEach(o -> responseList.add(new ProductResponse(o.getId(), o.getName(), o.getProductType(),
+                o.getDefaultPrice(), o.getDescription(), o.getImgName(),
+                convertBytesToMultipartFile(o.getImg(), o.getImgName()))));
     }
 }
